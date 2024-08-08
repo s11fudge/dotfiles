271eddd03ddc87e5bb551d6109fc387837178012
Frank Geusch <f.geusch@syseleven.de>
2024-08-08 10:41:10 +0200

    chore(): ongoning stuff

--- nvim/.config/nvim/lua/core/default_config.lua
+++ nvim/.config/nvim/lua/core/default_config.lua
@@ -30,8 +30,9 @@ M.ui = {
   telescope = { style = "borderless" }, -- borderless / bordered
 
   ------------------------------- nvchad_ui modules -----------------------------
+
   statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "default",
